d6c849af4b001698422b5bcc19769d168d9928bf
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-04-30 19:53:00 +0100

    add PackageManager enum

---
+++ InstallWith.Library/Enums/PackageManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstallWith.Library.Enums
+{
+    public enum PackageManager
+    {
+        APT,
+        AUR,
+        DNF,
+        Pacman,
+        Nix,
+        Homebrew,
+        Flatpak,
+        Snap,
+        Chocolatey,
+        Winget,
+        MacPorts,
+        NotSupported,
+        NotDetected
+    }
+}
