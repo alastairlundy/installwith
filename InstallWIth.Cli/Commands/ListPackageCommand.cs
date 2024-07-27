71c49a1f2924c64b1180801f25663693e7b32ed2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-07-27 13:17:40 +0100

    create command classes that will be needed later

---
+++ InstallWIth.Cli/Commands/ListPackageCommand.cs
@@ -0,0 +1,17 @@
+﻿using Spectre.Console.Cli;
+
+namespace InstallWith.Cli.Commands;
+
+public class ListPackageCommand : Command<ListPackageCommand.Settings>
+{
+
+    public class Settings : BaseSettings
+    {
+
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ InstallWIth.Cli/Commands/ListPackageUpdatesCommand.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console.Cli;
+
+namespace InstallWith.Cli.Commands;
+
+public class ListPackageUpdatesCommand : Command<ListPackageUpdatesCommand.Settings>
+{
+    public class Settings : BaseSettings
+    {
+        
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        throw new NotImplementedException();
+    }
+}
