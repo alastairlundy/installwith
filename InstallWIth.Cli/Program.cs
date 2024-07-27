bc21c99c76f83ae16c6ff6b9397344363b21eb66
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-07-27 13:16:35 +0100

    update dependencies and switch to Spectre

--- InstallWIth.Cli/Program.cs
+++ InstallWIth.Cli/Program.cs
@@ -16,106 +16,16 @@
 
 using System.Reflection;
 
-using AlastairLundy.Extensions.System.AssemblyExtensions;
-using AlastairLundy.Extensions.System.VersionExtensions;
-
 using InstallWith.Cli.localizations;
 
-using McMaster.Extensions.CommandLineUtils;
+using Spectre.Console.Cli;
 
-namespace InstallWith.Cli
+CommandApp app = new CommandApp();
+
+app.Configure(config =>
 {
-    internal class Program
-    {
-        static int Main(string[] args)
-        {
-            CommandLineApplication application = new CommandLineApplication();
 
-            application.Name = Resource.App_Name;
-            application.Description = Resource.App_Description;
-            
-            var help = application.HelpOption("-h|--help");
-            var version = application.Option("-v|--version", localizations.Resource.Command_Version_Description, CommandOptionType.NoValue);
-            var license = application.Option("-l|--license", Resource.Command_License_Description, CommandOptionType.NoValue);
+    config.UseAssemblyInformationalVersion();
+});
 
-            application.Command("install", installCommand =>
-            {
-                installCommand.Description = Resource.Command_Install_Description;
-
-                installCommand.OnExecute(() =>
-                {
-
-
-
-                });
-            });
-
-            application.Command("remove", removeCommand =>
-            {
-                removeCommand.Description = Resource.Command_Remove_Description;
-
-                removeCommand.OnExecute(() =>
-                {
-
-
-
-
-                });
-            });
-
-            application.Command("update", updateCommand =>
-            {
-                updateCommand.Description = Resource.Command_Update_Description;
-
-                updateCommand.OnExecute(() =>
-                {
-
-
-
-                });
-            });
-
-            application.Command("search", searchCommand =>
-            {
-                searchCommand.Description = Resource.Command_Search_Description;
-
-
-
-
-            });
-
-            application.Command("list", listCommand =>
-            {
-                listCommand.Description = Resource.Command_List_Description;
-
-
-
-
-            });
-
-            application.OnExecute(() =>
-            {
-                if (help.HasValue())
-                {
-                    application.ShowHelp();
-                }
-                if(version.HasValue())
-                {
-                    Console.WriteLine($@"{Resource.App_Name} {Assembly.GetExecutingAssembly().GetProjectVersion().GetFriendlyVersionToString()}");
-                }
-
-                if (license.HasValue())
-                {
-                    foreach(string line in File.ReadAllLines($"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}LICENSE.txt"))
-                    {
-                        Console.WriteLine(line);
-                    }
-
-                    Console.WriteLine();
-                }
-            });
-
-           return application.Execute(args);
-        }
-    }
-}
+return app.Run(args);
