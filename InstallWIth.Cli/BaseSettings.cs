2557797debf435b3da9b7242e979922828d89e51
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-07-27 13:16:45 +0100

    Create BaseSettings.cs

---
+++ InstallWIth.Cli/BaseSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
+
+namespace InstallWith.Cli;
+
+public class BaseSettings : CommandSettings
+{
+    [CommandArgument(0, "<Packages>")]
+    public string[]? Packages { get; init; }
+    
+    [CommandOption("-admin|--admin")]
+    [DefaultValue(false)]
+    public bool RunAsAdministrator { get; init; }
+    
+    [CommandOption("-with|--with:<PackageManager>")]
+    public string? PackageManagerToUse { get; init; }
+}
